d381ff918890a599821b1a8664ddd76b2914e9cb
Paul Thomas <paulthomas2107@gmail.com>
2022-10-31 09:46:44 +0000

    More....

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
-import Head from "next/head";
-import Image from "next/image";
-import formImage from '../public/form.png'
+import Head from 'next/head';
+import Image from 'next/image';
+import formImage from '../public/form.png';
+import { useFormik } from 'formik';
 
 export default function Home() {
+  // Formik Logic
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      country: 'United Kingdom',
+      terms: '',
+    },
+  });
+
   return (
     <div>
       <Head>
@@ -13,42 +24,93 @@ export default function Home() {
 
       <main className="h-screen items-center flex justify-center">
         <form className="bg-white flex rounded-lg w-1/2 font-latoRegular">
-          <div className='flex-1 text-gray-700 p-20'>
-            <h1 className='text-3xl pb-2 font-latoBold'>Let's get started 👋</h1>
-            <p className='text-lg text-gray-500'>Join our e-learning platform and unlock over 500+ courses
-              and digital assets ready to download.
+          <div className="flex-1 text-gray-700 p-20">
+            <h1 className="text-3xl pb-2 font-latoBold">
+              Let's get started 👋
+            </h1>
+            <p className="text-lg text-gray-500">
+              Join our e-learning platform and unlock over 500+ courses and
+              digital assets ready to download.
             </p>
-            <div className='mt-6' >
-              <div className='pb-4'>
-                <label className='block font-latoBold text-sm pb-2' htmlFor="name">Name</label>
-                <input className='border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500 w-full' 
-                  type="text" name="name" placeholder='Enter your name'/>
+            <div className="mt-6">
+              <div className="pb-4">
+                <label
+                  className="block font-latoBold text-sm pb-2"
+                  htmlFor="name"
+                >
+                  Name
+                </label>
+                <input
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500 w-full"
+                  type="text"
+                  name="name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  placeholder="Enter your name"
+                />
               </div>
-              <div className='pb-4'>
-                <label className='block font-latoBold text-sm pb-2' htmlFor="email">Email</label>
-                <input className='border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500 w-full' 
-                  type="email" name="email" placeholder='Enter your email'/>
+              <div className="pb-4">
+                <label
+                  className="block font-latoBold text-sm pb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500 w-full"
+                  type="email"
+                  name="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  placeholder="Enter your email"
+                />
               </div>
-              <div className='pb-4'>
-                <label className='block font-latoBold text-sm pb-2' htmlFor="country">Country</label>
-                <select name="country" className='border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500 w-full' >
+              <div className="pb-4">
+                <label
+                  className="block font-latoBold text-sm pb-2"
+                  htmlFor="country"
+                >
+                  Country
+                </label>
+                <select
+                  name="country"
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500 w-full"
+                  value={formik.values.country}
+                  onChange={formik.handleChange}
+                >
                   <option>Republic of Ireland</option>
-                  <option selected  >United Kingdom</option>
+                  <option selected>United Kingdom</option>
                   <option>United States</option>
                   <option>The Netherlands</option>
                 </select>
               </div>
-              <div className='pb-4'>
-                <label className='block font-latoBold text-sm pb-2' htmlFor="terms">Terms of service</label>
-                <div className='flex item-center gap-2'>
-                  <input type="checkbox" name="terms" value="checked"  className='h-5 w-5 text-teal-500 border-2 focus:border-teal-500 focus:ring-teal-500'/>
-                  <p className='text-sm font-latoBold text-gray-500'>
-                    I agree to the Terms and Service that my data will be taken and sold.
+              <div className="pb-4">
+                <label
+                  className="block font-latoBold text-sm pb-2"
+                  htmlFor="terms"
+                >
+                  Terms of service
+                </label>
+                <div className="flex item-center gap-2">
+                  <input
+                    type="checkbox"
+                    name="terms"
+                    value="checked"
+                    onChange={formik.handleChange}
+                    className="h-5 w-5 text-teal-500 border-2 focus:border-teal-500 focus:ring-teal-500"
+                  />
+                  <p className="text-sm font-latoBold text-gray-500">
+                    I agree to the Terms and Service that my data will be taken
+                    and sold.
                   </p>
                 </div>
               </div>
-              <button type='submit' className='bg-teal-500 font-latoBold text-sm text-white py-3 mt-6 rounded-lg w-full'>
-                Start Learning Today !</button>
+              <button
+                type="submit"
+                className="bg-teal-500 font-latoBold text-sm text-white py-3 mt-6 rounded-lg w-full"
+              >
+                Start Learning Today !
+              </button>
             </div>
           </div>
           <div className="relative flex-1">

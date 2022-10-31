55bfea49a73d83af0d1fe12de531b145d6c1c6f5
Paul Thomas <paulthomas2107@gmail.com>
2022-10-31 11:35:41 +0000

    motions

--- pages/success.js
+++ pages/success.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-
+import {motion as m} from 'framer-motion';
 
 export default function Success() {
 
@@ -7,13 +7,17 @@ export default function Success() {
 
     return (
         
-        <main className="h-screen items-center flex justify-center">
-            <div className="bg-white rounded-lg w-1/2 font-latoRegular text-gray-700 p-16">
-                <h1 className="text-3xl pb-4 font-latoBold">Thanks for the email {routers.query.name} ✨</h1>
+        <m.main 
+          initial={{opacity: 0}}
+          animate={{opacity: 1}} 
+          exit={{opacity: 0}}
+          className="h-screen items-center flex justify-center">
+            <div className="bg-white rounded-lg w-1/2 font-latoRegular text-center text-gray-700 p-16">
+                <h1 className="text-3xl pb-4 font-latoBold">Thanks for the email, {routers.query.name} ✨</h1>
                 <p className="text-lg text-gray-500">
-                    We have sent you an email over at {routers.query.email}. We will get back to you soon !
+                    We have sent you an email over to {routers.query.email}. We will get back to you soon !
                 </p>
             </div>
-        </main>
+        </m.main>
     )
 }
